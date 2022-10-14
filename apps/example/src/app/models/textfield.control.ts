95974c92f6d3baa535ac1294ceac37fddf83d342
ihor-panasiuk95 <ihor.panasiuk95@gmail.com>
2022-10-14 16:08:52 +0300

    Use DynamicControl in example up for simplicity

--- apps/example/src/app/models/textfield.control.ts
+++
@@ -1,8 +0,0 @@
-import { DynamicControl, ControlConfiguration } from '@skynet-ng/dynamic-form';
-import { TextfieldComponent } from '../components';
-
-export class TextfieldControl extends DynamicControl<TextfieldComponent> {
-    constructor(config: ControlConfiguration<any, any, string>) {
-        super(config, TextfieldComponent);
-    }
-}
